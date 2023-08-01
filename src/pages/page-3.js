a9c11c5dcb9f520f69206733eb7cfa44fe82bc2d
johson feng <fengyinchao1986@gmail.com>
2023-08-01 15:18:02 +0900

    blog site

---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM THE THIRD PAGE</h1>
+  </Layout>
+)
+
+export const Head = () => <Seo title="Page two" />
+
+export default SecondPage
